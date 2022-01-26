1686a9cd0bb085519afab321f15dc04f5883b6e8
nl <nldev00@pm.me>
2022-01-26 01:42:20 -0600

    update

--- addon/addon.ts
+++ addon/addon.ts
@@ -7,7 +7,17 @@ talents.SetMovable(true)
 
 talents.SetBackdropColor(0, 0, 0, 1)
 
-talents.SetPoint('CENTER', 'UIParent', 'CENTER', 0, 0)
+const btn = CreateFrame('Button', null, UIParent, 'UIPanelButtonTemplate')
+btn.SetPoint('CENTER')
+btn.SetSize(100, 40)
+btn.SetText('Click me')
+btn.SetScript('OnClick', function(self, button) {
+  console.log('You clicked me with ' + button)
+})
+
+console.log('hello')
+
+talents.SetPoint('CENTER', UIParent, 'CENTER', 0, 0)
 talents.Show()
 
 SendChatMessage('hello world', 'SAY')
