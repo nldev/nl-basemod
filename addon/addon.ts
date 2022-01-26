9ce2ca704847390527bed7314933501452293486
nl <nldev00@pm.me>
2022-01-26 01:42:36 -0600

    update

--- addon/addon.ts
+++ addon/addon.ts
@@ -11,7 +11,7 @@ const btn = CreateFrame('Button', null, UIParent, 'UIPanelButtonTemplate')
 btn.SetPoint('CENTER')
 btn.SetSize(100, 40)
 btn.SetText('Click me')
-btn.SetScript('OnClick', function(self, button) {
+btn.SetScript('OnClick', (_, button) => {
   console.log('You clicked me with ' + button)
 })
 
