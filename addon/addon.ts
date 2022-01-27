4cca105b68c5cc9a066cd6ce7a413d4f7238b19e
nl <nldev00@pm.me>
2022-01-26 18:48:37 -0600

    update

--- addon/addon.ts
+++ addon/addon.ts
@@ -1,23 +1,27 @@
-const talents = CreateFrame('Frame', 'foo')
-
-talents.SetWidth(800)
-talents.SetHeight(800)
+const talents = CreateFrame('Frame', 'foo', UIParent)
 
 talents.SetMovable(true)
 
-talents.SetBackdropColor(0, 0, 0, 1)
+talents.SetBackdrop({
+  bgFile: 'Interface/Tooltips/UI-Tooltip-Background',
+  edgeFile: 'Interface/Tooltips/UI-Tooltip-Border',
+  tile: true, tileSize: 16, edgeSize: 16,
+  insets: { left: 4, right: 4, top: 4, bottom: 4 },
+})
+
+talents.SetBackdropColor(0,0,0,1)
 
 const btn = CreateFrame('Button', null, UIParent, 'UIPanelButtonTemplate')
+
 btn.SetPoint('CENTER')
-btn.SetSize(100, 40)
+btn.SetSize(800, 800)
 btn.SetText('Click me')
 btn.SetScript('OnClick', (_, button) => {
   console.log('You clicked me with ' + button)
 })
 
-console.log('hello')
-
-talents.SetPoint('CENTER', UIParent, 'CENTER', 0, 0)
+talents.SetPoint('CENTER')
+talents.SetSize(100, 40)
 talents.Show()
 
 SendChatMessage('hello world', 'SAY')
