b7e92cf3f5e71c7c955b84a6d06eaaa9582bd08e
nl <nldev00@pm.me>
2022-01-26 19:45:17 -0600

    update

--- addon/addon.ts
+++ addon/addon.ts
@@ -1,6 +1,15 @@
-const talents = CreateFrame('Frame', 'foo', UIParent)
+const btn = CreateFrame('Button', null, UIParent, 'UIPanelButtonTemplate')
 
-talents.SetMovable(true)
+btn.SetPoint('TOPLEFT')
+btn.SetSize(100, 40)
+btn.SetText('Click me')
+btn.SetScript('OnClick', (_, button) => {
+  console.log('You clicked me with ' + button)
+})
+
+// -----
+
+const talents = CreateFrame('Frame', 'foo', UIParent)
 
 talents.SetBackdrop({
   bgFile: 'Interface/Tooltips/UI-Tooltip-Background',
@@ -9,20 +18,33 @@ talents.SetBackdrop({
   insets: { left: 4, right: 4, top: 4, bottom: 4 },
 })
 
-talents.SetBackdropColor(0,0,0,1)
+talents.SetBackdropColor(0, 0, 0, 1)
+talents.SetSize(800, 800)
+talents.SetPoint('CENTER')
 
-const btn = CreateFrame('Button', null, UIParent, 'UIPanelButtonTemplate')
+// -----
 
-btn.SetPoint('CENTER')
-btn.SetSize(800, 800)
-btn.SetText('Click me')
-btn.SetScript('OnClick', (_, button) => {
-  console.log('You clicked me with ' + button)
+const counter = CreateFrame('Frame', null, talents)
+
+counter.SetBackdrop({
+  bgFile: 'Interface/Tooltips/UI-Tooltip-Background',
+  edgeFile: 'Interface/Tooltips/UI-Tooltip-Border',
+  tile: true, tileSize: 16, edgeSize: 16,
+  insets: { left: 0, right: 0, top: 0, bottom: 0 },
 })
 
-talents.SetPoint('CENTER')
-talents.SetSize(100, 40)
-talents.Show()
+counter.SetBackdropColor(0, 0, 0, 1)
+counter.SetSize(20, 20)
+counter.SetPoint('TOPLEFT')
 
-SendChatMessage('hello world', 'SAY')
+// -----
+//
+const num = counter.CreateFontString()
+num.SetText('1')
+num.SetPoint('CENTER')
+
+// -----
+
+console.log('addon loaded')
+
 
