168435300588a9a4795a02a6a6bb5195f57cacff
nl <nldev00@pm.me>
2022-01-26 23:03:26 -0600

    update

--- addon/addon.ts
+++ addon/addon.ts
@@ -125,9 +125,8 @@ function TestFrame () {
   const frame = CreateFrame('Frame', null, UIParent)
 
   frame.SetBackdrop({
-    bgFile: 'Interface/Tooltips/UI-Tooltip-Background',
+    bgFile: 'Interface/Icons/Spell_Frost_WindWalkOn',
     edgeFile: 'Interface/Tooltips/UI-Tooltip-Border',
-    tile: true, tileSize: 16, edgeSize: 16,
     insets: { left: 4, right: 4, top: 4, bottom: 4 },
   })
 
