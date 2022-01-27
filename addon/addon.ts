c2efe1aefb67f29217d463f0b572f5ae6fca3ebd
nl <nldev00@pm.me>
2022-01-26 21:54:08 -0600

    update

--- addon/addon.ts
+++ addon/addon.ts
@@ -47,15 +47,25 @@ interface Mapping<T = any> {
   [key: string]: T
 }
 
+interface IGridItem {
+  frame: WoWAPI.Frame
+  index: number
+  height: number
+  width: number
+  x: number
+  y: number
+}
+
 class GridItem {
-  constructor (public frame: WoWAPI.Frame, public index: number) {
+  constructor (private params: IGridItem) {
   }
 }
 
 interface IGrid {
   itemsPerRow: number
   rowHeight: number
-  width: number
+  gridHeight: number
+  gridWidth: number
 }
 
 type GridOptions = Partial<IGrid>
@@ -63,23 +73,35 @@ type GridOptions = Partial<IGrid>
 const DEFAULT_GRID_OPTIONS: IGrid = {
   itemsPerRow: 4,
   rowHeight: 100,
-  width: 100,
+  gridHeight: 800,
+  gridWidth: 800,
 }
 
 class Grid {
   private list: GridItem[]
   private frame: WoWAPI.Frame
 
+  private itemHeight: number = 0
+  private itemWidth: number = 0
   private index: number = 0
-  private height: number = 0
-  private width: number = 0
+  private x: number = 0
+  private y: number = 0
+
+  private params: IGrid
 
   constructor (private options: GridOptions = DEFAULT_GRID_OPTIONS) {
-    this.options = { ...DEFAULT_GRID_OPTIONS, ...this.options }
+    this.params = { ...DEFAULT_GRID_OPTIONS, ...this.options }
   }
 
   public add (frame: WoWAPI.Frame) {
-    const item = new GridItem(frame, this.index)
+    const item = new GridItem({
+      frame,
+      index: this.index,
+      height: this.itemHeight,
+      width: this.itemWidth,
+      x: this.x,
+      y: this.y,
+    })
 
     if (this.index === this.options.itemsPerRow) {
       this.index = 0
