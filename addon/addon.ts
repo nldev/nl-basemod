b62b3a2856f996aef5187e6eb8a455e41874151d
nl <nldev00@pm.me>
2022-01-26 22:11:18 -0600

    update

--- addon/addon.ts
+++ addon/addon.ts
@@ -58,8 +58,22 @@ interface IGridItem {
 }
 
 class GridItem {
+  public frame: WoWAPI.Frame
+
   constructor (public params: IGridItem) {
-    // talents.SetPoint('CENTER', null, 'CENTER', this.x, this.y)
+    this.frame = CreateFrame('Frame', null, this.params.parent)
+
+    this.frame.SetBackdrop({
+      bgFile: 'Interface/Tooltips/UI-Tooltip-Background',
+      edgeFile: 'Interface/Tooltips/UI-Tooltip-Border',
+      tile: true, tileSize: 16, edgeSize: 16,
+      insets: { left: 4, right: 4, top: 4, bottom: 4 },
+    })
+
+    this.frame.SetBackdropColor(0, 0, 0, 1)
+    this.frame.SetSize(this.params.width, this.params.height)
+
+    this.params.child.SetParent(this.frame)
   }
 }
 
@@ -85,14 +99,15 @@ class Grid {
   public frame: WoWAPI.Frame
   public params: IGrid
 
-  public itemHeight: number = 0
   public itemWidth: number = 0
-  public index: number = 0
+  public index: number = -1
   public x: number = 0
   public y: number = 0
 
   constructor (options: GridOptions = DEFAULT_GRID_OPTIONS) {
     this.params = { ...DEFAULT_GRID_OPTIONS, ...options }
+
+    this.itemWidth = this.params.gridWidth / this.params.itemsPerRow
 
     this.frame = CreateFrame('Frame', null, UIParent)
 
@@ -108,19 +123,23 @@ class Grid {
   }
 
   public add (frame: WoWAPI.Frame) {
-    const item = new GridItem({
-      parent: this.frame,
-      child: frame,
-      index: this.index,
-      height: this.itemHeight,
-      width: this.itemWidth,
-      x: this.x,
-      y: this.y,
-    })
-
     if (this.index === (this.params.itemsPerRow - 1)) {
       this.index = 0
+
+      const item = new GridItem({
+        parent: this.frame,
+        child: frame,
+        index: this.index,
+        height: this.params.rowHeight,
+        width: this.itemWidth,
+        x: this.x,
+        y: this.y,
+      })
+
+      this.x += this.itemWidth
     } else {
+      this.x += 0
+      this.y += this.params.gridHeight
       this.index++
     }
   }
