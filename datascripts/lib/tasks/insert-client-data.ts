d5f00e265c8e8389f15301f06a7b77fd56534d9e
nl <nldev00@pm.me>
2022-01-27 18:42:29 -0600

    update

--- datascripts/lib/tasks/insert-client-data.ts
+++ datascripts/lib/tasks/insert-client-data.ts
@@ -11,35 +11,43 @@ export class InsertClientData extends NWTask {
   static readonly id = INSERT_CLIENT_DATA_TASK
 
   process (template: ClientDataTemplate) {
-    console.log('did a thing')
     const list: string[] = []
 
     for (let key of Object.keys(template.options)) {
-      console.log(key)
-
       const value = template.options[key]
+      const prefix = `const ${key} = `
 
       if (isNil(value)) {
+        list.push(`const ${key} = null`)
         continue
       }
 
       if (Array.isArray(value)) {
+        list.push(prefix + JSON.stringify(value))
         continue
       }
 
       if (typeof value === 'object') {
+        list.push(prefix + JSON.stringify(value))
         continue
       }
 
       if (typeof value === 'string') {
+        list.push(prefix + `\`${value}\``)
         continue
       }
 
       if (typeof value === 'number') {
+        list.push(prefix + value)
         continue
       }
 
       if (typeof value === 'boolean') {
+        list.push(
+          value
+            ? prefix + 'true'
+            : prefix + 'false'
+        )
         continue
       }
     }
