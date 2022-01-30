341f2ab6ad9a7b509be638073d3e1370119a50c8
nl <nldev00@pm.me>
2022-01-29 18:49:59 -0600

    update

--- addon/lib/index.ts
+++ addon/lib/index.ts
@@ -1,4 +1,13 @@
 import '../global'
+import { Root } from './root'
 
-export const root = new Root()
+export class UI {
+  public readonly root: Root
+
+  constructor () {
+    const root = new Root(root => {
+    })
+  }
+}
+
 
