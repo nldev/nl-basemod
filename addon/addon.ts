969db6f6d1bfaa887cb075f672e2518e7c42bb20
nl <nldev00@pm.me>
2022-01-31 01:49:26 -0600

    update

--- addon/addon.ts
+++ addon/addon.ts
@@ -1,20 +1,24 @@
 import { render } from './basemod/app'
 import { Frame } from './basemod/component'
-import { Scroll } from './basemod/components/scroll'
+// import { Scroll } from './basemod/components/scroll'
 
-render($ => Scroll({ name: 'bar' })
-  .Point('CENTER')
-  .Size(500, 500)
-  .Backdrop()
-  .Children([
-    Frame({}, [
-      Frame({}, [
-        Frame(),
-        Frame(),
-        Frame(),
-      ]),
-    ]),
-  ])
+console.log('helo one!')
+
+render($ => Frame({ name: 'bar' })
+  // .Point('CENTER')
+  // .Size(500, 500)
+  // .Backdrop()
+  // .Children([
+  //   Frame({}, [
+  //     Frame({}, [
+  //       Frame(),
+  //       Frame(),
+  //       Frame(),
+  //     ]),
+  //   ]),
+  // ])
   // .Click('LeftButtonDown', () => console.log('hello'))
+  // .Run(() => console.log('hello world'))
 )
 
+console.log('helo!')
