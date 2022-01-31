d85678031d2ab39fc79393514553bf7e1a8bd358
nl <nldev00@pm.me>
2022-01-30 18:26:53 -0600

    update

--- addon/addon.ts
+++ addon/addon.ts
@@ -1,7 +1,8 @@
+// import { container } from './basemod/index'
 import { Container } from './basemod/container'
 
-
 export const container = new Container()
+
 
 // const container = new Container($ => {
 //   console.log($.playerInfo.name)
--- addon/basemod/container.ts
+++ addon/basemod/container.ts
@@ -1,32 +1,74 @@
-import '../global'
-
 interface PlayerInfo {
   name: string
 }
 
+// export class Container {
+//   public root: WoWAPI.Frame
+//
+//   constructor () {
+//     this.root = CreateFrame('Frame', 'root', UIParent)
+//
+//     this.root.SetScript('OnUpdate', () => this.load())
+//   }
+//
+//   load () {
+//     const player = UnitGUID('player')
+//
+//     if (player) {
+//       const info = GetPlayerInfoByGUID(player)
+//
+//       if (info[0])
+//         return this.ready()
+//     }
+//
+//     return this.load()
+//   }
+//
+//   ready () {
+//     console.log('got to ready state')
+//   }
+// }
+
 export class Container {
+  protected isLoaded: boolean = false
+  protected isInit: boolean = false
+
+  public root: WoWAPI.Frame
+  public playerInfo: PlayerInfo
+  // public components: Mapping<Component>
+
   constructor () {
-    console.log('got here')
-    this.load()
+    this.root = CreateFrame('Frame')
+
+    this.root.SetScript('OnUpdate', () => this.load())
   }
 
-  load () {
+  protected load () {
+    if (this.isLoaded)
+      return
+
     const player = UnitGUID('player')
 
-    if (!player)
-      return this.load()
+    if (player) {
+      const info = GetPlayerInfoByGUID(player)
 
-    const info = GetPlayerInfoByGUID(player)
+      if (info[0]) {
+        console.log('hello')
 
-    if (!info[0])
-      return this.load()
+        // this.playerInfo = {
+        //   name: info[5].toLowerCase(),
+        //   chrRace: info[2].toUpperCase() as CharacterRace,
+        //   chrClass: info[0].toUpperCase() as CharacterClass,
+        //   level: info[4],
+        // }
 
-      console.log('got here again')
+        this.isLoaded = true
 
-    return this.ready()
-  }
+        console.log('loaded')
+        // return this.onInit(this)
+      }
 
-  ready () {
-    console.log('got to ready state')
+      this.load()
+    }
   }
 }
