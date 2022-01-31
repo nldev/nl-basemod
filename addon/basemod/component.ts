f2fedfefb3688bf51e11ed499e4d7fb668bf54dc
nl <nldev00@pm.me>
2022-01-30 21:56:15 -0600

    update

--- addon/basemod/component.ts
+++ addon/basemod/component.ts
@@ -1,41 +1,6 @@
 import { Unique } from './utils'
 import { Get } from './app'
 
-export interface ComponentOptions {
-  name?: string
-  isPrefix?: boolean
-}
-
-export class Component<
-  O extends ComponentOptions = ComponentOptions,
-  T extends WoWAPI.UIObject = WoWAPI.Frame,
-> {
-  public object: T
-
-  constructor (protected options?: O) {}
-}
-
-export interface FrameOptions extends ComponentOptions {
-}
-
-const DEFAULT_FRAME_OPTIONS = {}
-
-export class Frame extends Component {
-  constructor (protected options: FrameOptions = DEFAULT_FRAME_OPTIONS) {
-    super(options)
-
-    const $ = Get()
-
-    const component = CreateFrame(
-      'Frame',
-      options.name
-        ? (options.isPrefix ? Unique(options.name) : options.name)
-        : null,
-      $.root,
-    )
-  }
-}
-
 interface Color {
   red: number
   green: number
@@ -43,20 +8,23 @@ interface Color {
   alpha: number
 }
 
+type ColorOptions = Partial<Color>
+
 interface Backdrop {
-  bgFile?: string
-  edgeFile?: string
-  tile?: boolean
-  tileSize?: number
-  edgeSize?: number
-  insets?: {
-    left?: number
-    right?: number
-    top?: number
-    bottom?: number
+  bgFile: string
+  edgeFile: string
+  tile: boolean
+  tileSize: number
+  edgeSize: number
+  insets: {
+    left: number
+    right: number
+    top: number
+    bottom: number
   },
-  Color?: Color
 }
+
+type BackdropOptions = Partial<Backdrop>
 
 // this.frame.SetBackdrop({
 //   bgFile: 'Interface/Tooltips/UI-Tooltip-Background',
@@ -77,10 +45,66 @@ const DEFAULT_BACKDROP = {
     top: 4,
     bottom: 4,
   },
-  color: { red: 0, green: 0, blue: 0, alpha: 1 },
 }
 
-function SetBackdrop (backdrop: Backdrop = DEFAULT_BACKDROP) {
-  this.frame
+const DEFAULT_COLOR = {
+  red: 0,
+  green: 0,
+  blue: 0,
+  alpha: 1,
 }
 
+export interface ComponentOptions {
+  name?: string
+  isPrefix?: boolean
+}
+
+export class Component<
+  O extends ComponentOptions = ComponentOptions,
+  T extends WoWAPI.UIObject = WoWAPI.Frame,
+> {
+  public ref: T
+
+  constructor (protected options?: O) {}
+}
+
+export interface FrameOptions extends ComponentOptions {
+  bgOptions?: BackdropOptions
+  colorOptions?: ColorOptions
+}
+
+const DEFAULT_FRAME_OPTIONS = {
+  bgOptions: DEFAULT_BACKDROP,
+  colorOptions: DEFAULT_COLOR,
+}
+
+export class Frame extends Component {
+  constructor (protected options: FrameOptions = DEFAULT_FRAME_OPTIONS) {
+    super(options)
+
+    const $ = Get()
+
+    this.ref = CreateFrame(
+      'Frame',
+      options.name
+        ? (options.isPrefix ? Unique(options.name) : options.name)
+        : null,
+      $.root,
+    )
+  }
+
+  Backdrop (bgOptions: BackdropOptions = DEFAULT_BACKDROP, colorOptions: ColorOptions = DEFAULT_COLOR) {
+    const backdrop: Backdrop = {
+      ...DEFAULT_BACKDROP,
+      ...bgOptions,
+    }
+    this.ref.SetBackdrop(backdrop)
+
+    const color: Color = {
+      ...DEFAULT_COLOR,
+      ...colorOptions,
+    }
+
+    this.ref.SetBackdropColor(color.red, color.green, color.blue, color.alpha)
+  }
+}
