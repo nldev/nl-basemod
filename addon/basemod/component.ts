652a3c2f3ecc5cf05d64a38fe49220e1043187e5
nl <nldev00@pm.me>
2022-01-30 21:35:41 -0600

    update

--- addon/basemod/component.ts
+++ addon/basemod/component.ts
@@ -12,8 +12,34 @@ const DEFAULT_FRAME_OPTIONS = {}
 export const Frame: Component = (options = DEFAULT_FRAME_OPTIONS) => {
   const $ = Get()
 
+  this.frame.SetBackdrop({
+    bgFile: 'Interface/Tooltips/UI-Tooltip-Background',
+    edgeFile: 'Interface/Tooltips/UI-Tooltip-Border',
+    tile: true, tileSize: 16, edgeSize: 16,
+    insets: { left: 4, right: 4, top: 4, bottom: 4 },
+  })
+
+
   const component = CreateFrame('Frame', null, $.root)
 
+
   return component
+}
+
+interface Backdrop {
+  bgFile?: string
+  edgeFile?: string
+  tile?: boolean
+  tileSize?: number
+  edgeSize?: number
+  insets?: {
+    left?: number
+    right?: number
+    top?: number
+    bottom?: number
+  }
+}
+
+function SetBackdrop () {
 }
 
