6d1650bc7777da9ef7943eb5ddf27905ea2e4bbc
nl <nldev00@pm.me>
2022-01-31 16:31:09 -0600

    update

--- addon/basemod/component.ts
+++ addon/basemod/component.ts
@@ -281,21 +281,16 @@ export class FrameElement<O extends FrameOptions = FrameOptions> extends Element
   }
 
   public Drag (type: WoWAPI.MouseButton, startHandler?: FrameDragStartHandler, stopHandler?: FrameDragStopHandler) {
-    console.log('here')
     this.ref.EnableMouse(true)
-    console.log('here')
     this.ref.RegisterForDrag(type)
-    console.log('here')
     this.ref.SetMovable(true)
-    console.log('here')
 
     this.ref.SetScript('OnDragStart', (frame, type) => {
-      console.log('start')
       const state = { preventDefault: false }
 
-      const preventDefault = () => state.preventDefault = true
+      const preventDefault = () => { state.preventDefault = true }
 
-      startHandler(frame, type, preventDefault)
+      startHandler(frame, type, () => preventDefault())
 
       console.log(type)
 
@@ -304,12 +299,11 @@ export class FrameElement<O extends FrameOptions = FrameOptions> extends Element
     })
 
     this.ref.SetScript('OnDragStop', (frame) => {
-      console.log('stop')
       const state = { preventDefault: false }
 
       const preventDefault = () => state.preventDefault = true
 
-      stopHandler(frame, type, preventDefault)
+      stopHandler(frame, type, () => preventDefault())
 
       console.log(type)
 
