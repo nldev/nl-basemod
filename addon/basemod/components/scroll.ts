0e44a10003f307097e730f8355583cd4b595c1c3
nl <nldev00@pm.me>
2022-01-31 16:29:41 -0600

    update

--- addon/basemod/components/scroll.ts
+++ addon/basemod/components/scroll.ts
@@ -21,7 +21,6 @@ export class ScrollElement extends FrameElement {
     // this.ref.SetMovable(true)
     // this.ref.SetScript('OnDragStart', f => f.StartMoving())
     // this.ref.SetScript('OnDragStop', f => f.StopMovingOrSizing())
-    this.Drag('RightButton')
 
     const scrollframe = CreateFrame('ScrollFrame', 'scrollframe', null, 'UIPanelScrollFrameTemplate')
     const scrollchild = CreateFrame('Frame', 'scrollchild')
