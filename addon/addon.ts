8c7cc6c3163da38ead901d349ef1325c04376e4b
nl <nldev00@pm.me>
2022-02-17 01:19:47 -0600

    update

--- addon/addon.ts
+++ addon/addon.ts
@@ -538,7 +538,7 @@ export const Talent: Component<TalentOptions, TalentState, TalentFns> = options 
   // onClick
   frame.inner.SetScript('OnMouseDown', (_, button) => {
     const remainder = app.talentInfo.max - app.talentInfo.used
-    if (button === 'LeftButton' && !frame.state.isActive && (options.spell.cost > remainder))
+    if (button === 'LeftButton' && !frame.state.isActive && (options.spell.cost <= remainder))
       frame.fns.requestActivate()
 
     if (button === 'RightButton' && frame.state.isActive)
