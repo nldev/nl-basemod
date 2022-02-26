dec7e9b1595c363dcdd173ccc3c4db3ac5d727a2
nl <nldev00@pm.me>
2022-02-25 21:18:20 -0600

    update

--- addon/addon.ts
+++ addon/addon.ts
@@ -810,6 +810,9 @@ export const LootItem: Component<
 
   // icon
   const icon = Frame({ name: frame.ref.GetName() + '-icon', parent: frame })
+
+  icon.ref.SetSize(40, 40)
+
   icon.ref.SetBackdrop({
     edgeFile: 'Interface/Tooltips/UI-Tooltip-Border',
     tile: true,
