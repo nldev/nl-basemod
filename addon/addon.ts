96fe125861b1e8c6fd65e3d55f7e117d922a39ea
nl <nldev00@pm.me>
2022-02-25 21:41:42 -0600

    update

--- addon/addon.ts
+++ addon/addon.ts
@@ -815,7 +815,7 @@ export const LootItem: Component<
 
   icon.ref.SetBackdropColor(0, 0, 0, 1)
 
-  const texture = icon.ref.CreateTexture()
+  const texture = icon.ref.CreateTexture(`${frame.ref.GetName()}-texture`)
 
   texture.SetTexture(GetItemIcon(options.itemId))
   texture.SetAllPoints()
