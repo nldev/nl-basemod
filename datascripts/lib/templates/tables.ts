6bb47bcdd5e696effb20e82d7490927a24983300
nl <nldev00@pm.me>
2022-02-26 17:41:52 -0600

    update

--- datascripts/lib/templates/tables.ts
+++ datascripts/lib/templates/tables.ts
@@ -4,54 +4,41 @@ import { SQLTable } from '../types'
 
 export const TABLES = createTemplates<SQLTable>(CREATE_TABLE_TASK, [
   {
-    name: 'addon-data',
+    name: 'addon_data',
     columns: [
       {
-        name: 'id',
+        name: 'entry',
         type: 'mediumint',
         typeParams: {
-          size: 32,
+          size: 16,
         },
+        isPrimaryKey: true,
+        isNotNullable: true,
+        isAutoIncrement: true,
       },
       {
         name: 'playerGuid',
-        type: 'text',
-        typeParams: {
-          size: 32,
-        },
-        isPrimaryKey: true,
+        type: 'mediumtext',
         isNotNullable: true,
       },
       {
         name: 't',
-        type: 'text',
-        typeParams: {
-          size: 32,
-        },
+        type: 'mediumtext',
         isNotNullable: true,
       },
       {
         name: 'type',
-        type: 'text',
-        typeParams: {
-          size: 32,
-        },
+        type: 'mediumtext',
         isNotNullable: true,
       },
       {
         name: 'key',
-        type: 'text',
-        typeParams: {
-          size: 32,
-        },
+        type: 'mediumtext',
         isNotNullable: true,
       },
       {
         name: 'value',
-        type: 'text',
-        typeParams: {
-          size: 500,
-        },
+        type: 'mediumtext',
         isNotNullable: true,
       },
     ],
