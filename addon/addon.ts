6d053edfa73bd4d56674a35f34b05cd77c131344
nl <nldev00@pm.me>
2022-02-26 21:00:07 -0600

    update

--- addon/addon.ts
+++ addon/addon.ts
@@ -1040,12 +1040,8 @@ export const Loot: Component<LootOptions, LootState, LootFns> = () => {
   let a3 = app.store.Get('STORE_TYPE_CHARACTER', 'loot-f3')
   let a4 = app.store.Get('STORE_TYPE_CHARACTER', 'loot-f4')
   let a5 = app.store.Get('STORE_TYPE_CHARACTER', 'loot-f5')
-  console.log(a1)
-  console.log(a3)
-  console.log(a4)
-  console.log(a5)
 
-  if (a1 !== '' && !a1) {
+  if (!a1) {
     console.log('here')
     padding.ref.SetPoint('CENTER')
     let [
