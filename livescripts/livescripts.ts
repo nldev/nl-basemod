328e28c44c2e3b64791b8150039fc8a5073c4037
nl <nldev00@pm.me>
2022-02-26 18:40:38 -0600

    update

--- livescripts/livescripts.ts
+++ livescripts/livescripts.ts
@@ -571,25 +571,81 @@ function EasyLoot (events: TSEvents) {
 
 function Store (events: TSEvents) {
   events.Player.OnWhisper((sender, _, message) => {
-    const opcode = Opcode('loot-item')
+    const opcode = Opcode('store-set')
     const str = message.get()
     if (!str.includes(opcode))
      return
     const a = str.substr(opcode.length).split(' ')
     if (!a[0])
       return
-    sender.AddItem(ToUInt32(a[0]), ToUInt32(a[1]))
+    const primitive = a[0]
+    const type = a[1]
+    const key = a[2]
+    const value = a[3]
+    sender.SendAddonMessage(
+      'store-get',
+      `${primitive} ${type} ${key} ${value}`,
+      0,
+      sender,
+    )
+  })
+  events.Player.OnWhisper((sender, _, message) => {
+    const opcode = Opcode('store-init')
+    const str = message.get()
+    if (!str.includes(opcode))
+     return
+    const playerGuid = sender.GetGUID()
+    const a = QueryWorld(`
+      select * from __addon_data where playerGuid = ${playerGuid};
+    `)
+    while (a.GetRow()) {
+      const primitive = a.GetString(2)
+      const type = a.GetString(3)
+      const key = a.GetString(4)
+      const value = a.GetString(5)
+      sender.SendAddonMessage(
+        'store-get',
+        `${primitive} ${type} ${key} ${value}`,
+        0,
+        sender,
+      )
+    }
+    sender.SendAddonMessage(
+      'store-init-success',
+      '',
+      0,
+      sender,
+    )
   })
 
   events.Player.OnWhisper((sender, _, message) => {
-    const opcode = Opcode('loot-item')
+    const opcode = Opcode('store-set')
     const str = message.get()
     if (!str.includes(opcode))
      return
-    const a = str.substr(opcode.length).split(' ')
-    if (!a[0])
-      return
-    sender.AddItem(ToUInt32(a[0]), ToUInt32(a[1]))
+    const w = str.substr(opcode.length).split(' ')
+    const primitive = w[0]
+    const type = w[1]
+    const key = w[2]
+    const value = w[3]
+    const playerGuid = sender.GetGUID()
+    const a = QueryWorld(`
+      select * from __addon_data where playerGuid = ${playerGuid} and type = "${type}" and key = "${key}";
+    `)
+    let id = -1
+    while (a.GetRow()) {
+      id = a.GetUInt32(0)
+    }
+    if (id === -1) {
+      QueryWorld(`
+        insert into __addon_data (playerGuid, primitive, type, key, value) value ("${playerGuid}", "${primitive}", "${type}", "${key}", "${value}");
+      `)
+    } else {
+      QueryWorld(`
+        update __addon_data set primitive = "${primitive}", type = "${type}", key = "${key}", value = "${value}" where id = ${id};
+      `)
+    }
+    sender.SendBroadcastMessage(`${primitive} ${type} ${key} ${value}`)
   })
 }
 
