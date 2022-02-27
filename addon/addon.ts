54d3d005d048a120aa48eb97cc865a737132ba8a
nl <nldev00@pm.me>
2022-02-26 21:03:57 -0600

    update

--- addon/addon.ts
+++ addon/addon.ts
@@ -178,7 +178,7 @@ export class Store {
         ? Number(value)
         : t === 'null'
         ? null
-        : t
+        : value
     })
 
     Events.ChatInfo.OnChatMsgAddon(app.root.ref, prefix => {
