8a929ced88234f5517dbc3d1618c80966f5e49d7
nl <nldev00@pm.me>
2022-03-02 01:08:43 -0600

    update

--- datascripts/datascripts.ts
+++ datascripts/datascripts.ts
@@ -303,8 +303,8 @@ function main () {
         const sta = Math.floor(STATS[CLASS_IDS[classId]].staMin + (i * STATS[CLASS_IDS[classId]].staInc))
         const str = Math.floor(STATS[CLASS_IDS[classId]].strMin + (i * STATS[CLASS_IDS[classId]].strInc))
         const int = Math.floor(STATS[CLASS_IDS[classId]].intMin + (i * STATS[CLASS_IDS[classId]].intInc))
-        $.sql.Databases.world_source.write(`
-          insert into player_levelstats ("race", "class", "level", "str", "agi", "sta", "inte", "spi") values (${race}, ${cls}, ${level}, ${str}, ${agi}, ${sta}, ${int}, ${spi});
+        $.sql.Databases.world_source.writeLate(`
+          insert into player_levelstats (\`race\`, \`class\`, \`level\`, \`str\`, \`agi\`, \`sta\`, \`inte\`, \`spi\`) values (${race}, ${cls}, ${level}, ${str}, ${agi}, ${sta}, ${int}, ${spi});
         `)
       }
 
