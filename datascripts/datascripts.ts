2fd776f15ed0c2847fb42f22ec1d1115a69ecc13
nl <nldev00@pm.me>
2022-03-02 00:32:52 -0600

    update

--- datascripts/datascripts.ts
+++ datascripts/datascripts.ts
@@ -293,6 +293,7 @@ function main () {
   for (const raceId of Object.keys(RACE_IDS))
     for (const classId of Object.keys(CLASS_IDS))
       for (let i = 0; i < 99; i++) {
+        console.log($.sql.player_levelstats.query({ race: RACE_IDS[raceId], class: STATS[CLASS_IDS[classId]], level: i + 1 }).class)
         $.sql.player_levelstats.query({ race: RACE_IDS[raceId], class: STATS[CLASS_IDS[classId]], level: i + 1 })
           .agi.set(Math.floor(STATS[CLASS_IDS[classId]].agiMin + (i * STATS[CLASS_IDS[classId]].agiInc)))
           .spi.set(Math.floor(STATS[CLASS_IDS[classId]].spiMin + (i * STATS[CLASS_IDS[classId]].spiInc)))
