750fc13055881853432707da76abe653e45f6181
nl <nldev00@pm.me>
2022-03-06 07:11:29 -0600

    update

--- datascripts/datascripts.ts
+++ datascripts/datascripts.ts
@@ -1,235 +1,19 @@
 import { std } from 'wow/wotlk'
 import { Builder } from './basemod'
-import { CLASS_IDS, RACE_IDS, ROGUE } from './basemod/constants'
 import { createClassMask, createRaceMask, resolveDuration } from './basemod/utils'
-import * as fs from 'fs'
-const STATS: any = {
-  1: {
-    mpMax: 0,
-    mpMin: 0,
-    hpMax: 1689,
-    hpMin: 839,
-    strMax: 128,
-    strMin: 98,
-    agiMax: 87,
-    agiMin: 68,
-    staMax: 116,
-    staMin: 88,
-    intMax: 34,
-    intMin: 30,
-    spiMax: 50,
-    spiMin: 40,
-  },
-  2: {
-    mpMax: 1512,
-    mpMin: 1137,
-    hpMax: 1381,
-    hpMin: 786,
-    strMax: 109,
-    strMin: 83,
-    agiMax: 68,
-    agiMin: 54,
-    staMax: 105,
-    staMin: 81,
-    intMax: 75,
-    intMin: 59,
-    spiMax: 78,
-    spiMin: 61,
-  },
-  3: {
-    mpMax: 1720,
-    mpMin: 1270,
-    hpMax: 1467,
-    hpMin: 842,
-    strMax: 61,
-    strMin: 49,
-    agiMax: 133,
-    agiMin: 101,
-    staMax: 95,
-    staMin: 73,
-    intMax: 70,
-    intMin: 56,
-    spiMax: 74,
-    spiMin: 58,
-  },
-  4: {
-    mpMax: 0,
-    mpMin: 0,
-    hpMax: 1523,
-    hpMin: 878,
-    strMax: 84,
-    strMin: 66,
-    agiMax: 139,
-    agiMin: 105,
-    staMax: 79,
-    staMin: 62,
-    intMax: 39,
-    intMin: 34,
-    spiMax: 54,
-    spiMin: 44,
-  },
-  5: {
-    mpMax: 1376,
-    mpMin: 1046,
-    hpMax: 1397,
-    hpMin: 792,
-    strMax: 36,
-    strMin: 32,
-    agiMax: 45,
-    agiMin: 38,
-    staMax: 54,
-    staMin: 44,
-    intMax: 127,
-    intMin: 96,
-    spiMax: 133,
-    spiMin: 101,
-  },
-  6: {
-    mpMax: 1,
-    mpMin: 1,
-    hpMax: 1,
-    hpMin: 1,
-    strMax: 1,
-    strMin: 1,
-    agiMax: 1,
-    agiMin: 1,
-    staMax: 1,
-    staMin: 1,
-    intMax: 1,
-    intMin: 1,
-    spiMax: 1,
-    spiMin: 1,
-  },
-  7: {
-    mpMax: 1520,
-    mpMin: 760,
-    hpMax: 1423,
-    hpMin: 1115,
-    strMax: 92,
-    strMin: 72,
-    agiMax: 58,
-    agiMin: 46,
-    staMax: 99,
-    staMin: 76,
-    intMax: 93,
-    intMin: 71,
-    spiMax: 105,
-    spiMin: 81,
-  },
-  8: {
-    mpMax: 1213,
-    mpMin: 958,
-    hpMax: 1370,
-    hpMin: 775,
-    strMax: 31,
-    strMin: 27,
-    agiMax: 38,
-    agiMin: 33,
-    staMax: 46,
-    staMin: 38,
-    intMax: 132,
-    intMin: 100,
-    spiMax: 128,
-    spiMin: 97,
-  },
-  9: {
-    mpMax: 1373,
-    mpMin: 1043,
-    hpMax: 1414,
-    hpMin: 799,
-    strMax: 48,
-    strMin: 40,
-    agiMax: 51,
-    agiMin: 44,
-    staMax: 68,
-    staMin: 54,
-    intMax: 117,
-    intMin: 89,
-    spiMax: 116,
-    spiMin: 89,
-  },
-  11: {
-    mpMax: 1244,
-    mpMin: 959,
-    hpMax: 1483,
-    hpMin: 858,
-    strMax: 71,
-    strMin: 57,
-    agiMax: 66,
-    agiMin: 53,
-    staMax: 70,
-    staMin: 57,
-    intMax: 102,
-    intMin: 78,
-    spiMax: 113,
-    spiMin: 87,
-  },
-}
-
-// FIXME: move this
-function CreatePlaceholderEnchants () {
-  const special = std.DBC.SpellItemEnchantment.add(9999)
-  special.Name.enGB.set('[special]')
-  special.Flags.set(0)
-  special.Effect.set([0, 0, 0, 0, 0])
-  special.ItemVisual.set(0)
-  for (let i = 0; i <= 99999; i++) {
-    const enchant = std.DBC.SpellItemEnchantment.add(9996 + i)
-    const prop = std.DBC.ItemRandomProperties.add(10000 + i)
-    enchant.Name.enGB.set('[placeholder ' + i + ']')
-    enchant.Flags.set(0)
-    enchant.Effect.set([0, 0, 0, 0, 0])
-    enchant.ItemVisual.set(0)
-    prop.Name.set('')
-    prop.Name2.enGB.set('')
-    prop.Enchantment.set([i])
-  }
-}
-
-CreatePlaceholderEnchants()
-
-// ability
-// passive
-// utility
-
-// FIXME: move this
-function StarterSpells ($: Builder) {
-  const ability = $.std.DBC.SkillLine.add(9000)
-  const passive = $.std.DBC.SkillLine.add(9001)
-  const utility = $.std.DBC.SkillLine.add(9002)
-  ability.DisplayName.enGB.set('Ability')
-  passive.DisplayName.enGB.set('Passive')
-  utility.DisplayName.enGB.set('Utility')
-  $.std.Spells.load(5938).SkillLines.clearClass('ROGUE')
-  $.std.Spells.load(5938).SkillLines.enable('ROGUE', 'GNOME')
-  $.std.Spells.load(5938).SkillLines.add(9000)
-  $.std.DBC.SkillLineAbility.add(90000, {
-    Spell: 5938,
-    RaceMask: 1791,
-    ClassMask: 8,
-    AcquireMethod: 2,
-    SkillLine: 9000,
-  })
-  $.std.DBC.SkillLineAbility.queryAll({}).forEach(v => v.CharacterPoints)
-  // console.log('skill line abilities')
-  // $.std.DBC.SkillLineAbility.queryAll({}).forEach(v => {
-  //   const spell = $.std.Spells.load(v.Spell.get())
-  //   if (spell && spell.Name && spell.Name.enGB)
-  //     console.log(spell.Name.enGB.get())
-  // })
-
-  // console.log('skill lines')
-  // $.std.DBC.SkillLine.queryAll({}).forEach(v => console.log(v.DisplayName.enGB.get()))
-}
-
-// ---
 
 function main () {
   const $ = new Builder()
+
   // FIXME: give weapon skills
   // FIXME: set config to max weapon skills
 
   $.init()
+
+  temp($)
+}
+
+const temp = ($: Builder) => {
   StarterSpells($)
 
   const b = $.std.Items.load(2504)
@@ -820,16 +604,222 @@ function main () {
 
 main()
 
-const NPC_EXAMPLE = {
-  combat: ['lunge-attack', '25-cold-resist'],
+const STATS: any = {
+  1: {
+    mpMax: 0,
+    mpMin: 0,
+    hpMax: 1689,
+    hpMin: 839,
+    strMax: 128,
+    strMin: 98,
+    agiMax: 87,
+    agiMin: 68,
+    staMax: 116,
+    staMin: 88,
+    intMax: 34,
+    intMin: 30,
+    spiMax: 50,
+    spiMin: 40,
+  },
+  2: {
+    mpMax: 1512,
+    mpMin: 1137,
+    hpMax: 1381,
+    hpMin: 786,
+    strMax: 109,
+    strMin: 83,
+    agiMax: 68,
+    agiMin: 54,
+    staMax: 105,
+    staMin: 81,
+    intMax: 75,
+    intMin: 59,
+    spiMax: 78,
+    spiMin: 61,
+  },
+  3: {
+    mpMax: 1720,
+    mpMin: 1270,
+    hpMax: 1467,
+    hpMin: 842,
+    strMax: 61,
+    strMin: 49,
+    agiMax: 133,
+    agiMin: 101,
+    staMax: 95,
+    staMin: 73,
+    intMax: 70,
+    intMin: 56,
+    spiMax: 74,
+    spiMin: 58,
+  },
+  4: {
+    mpMax: 0,
+    mpMin: 0,
+    hpMax: 1523,
+    hpMin: 878,
+    strMax: 84,
+    strMin: 66,
+    agiMax: 139,
+    agiMin: 105,
+    staMax: 79,
+    staMin: 62,
+    intMax: 39,
+    intMin: 34,
+    spiMax: 54,
+    spiMin: 44,
+  },
+  5: {
+    mpMax: 1376,
+    mpMin: 1046,
+    hpMax: 1397,
+    hpMin: 792,
+    strMax: 36,
+    strMin: 32,
+    agiMax: 45,
+    agiMin: 38,
+    staMax: 54,
+    staMin: 44,
+    intMax: 127,
+    intMin: 96,
+    spiMax: 133,
+    spiMin: 101,
+  },
+  6: {
+    mpMax: 1,
+    mpMin: 1,
+    hpMax: 1,
+    hpMin: 1,
+    strMax: 1,
+    strMin: 1,
+    agiMax: 1,
+    agiMin: 1,
+    staMax: 1,
+    staMin: 1,
+    intMax: 1,
+    intMin: 1,
+    spiMax: 1,
+    spiMin: 1,
+  },
+  7: {
+    mpMax: 1520,
+    mpMin: 760,
+    hpMax: 1423,
+    hpMin: 1115,
+    strMax: 92,
+    strMin: 72,
+    agiMax: 58,
+    agiMin: 46,
+    staMax: 99,
+    staMin: 76,
+    intMax: 93,
+    intMin: 71,
+    spiMax: 105,
+    spiMin: 81,
+  },
+  8: {
+    mpMax: 1213,
+    mpMin: 958,
+    hpMax: 1370,
+    hpMin: 775,
+    strMax: 31,
+    strMin: 27,
+    agiMax: 38,
+    agiMin: 33,
+    staMax: 46,
+    staMin: 38,
+    intMax: 132,
+    intMin: 100,
+    spiMax: 128,
+    spiMin: 97,
+  },
+  9: {
+    mpMax: 1373,
+    mpMin: 1043,
+    hpMax: 1414,
+    hpMin: 799,
+    strMax: 48,
+    strMin: 40,
+    agiMax: 51,
+    agiMin: 44,
+    staMax: 68,
+    staMin: 54,
+    intMax: 117,
+    intMin: 89,
+    spiMax: 116,
+    spiMin: 89,
+  },
+  11: {
+    mpMax: 1244,
+    mpMin: 959,
+    hpMax: 1483,
+    hpMin: 858,
+    strMax: 71,
+    strMin: 57,
+    agiMax: 66,
+    agiMin: 53,
+    staMax: 70,
+    staMin: 57,
+    intMax: 102,
+    intMin: 78,
+    spiMax: 113,
+    spiMin: 87,
+  },
 }
 
-//   paladin:
-//   hunter:
-//   rogue:
-//   priest:
-//   shaman:
-//   mage:
-//   warlock:
-//   druid:
+// FIXME: move this
+function CreatePlaceholderEnchants () {
+  const special = std.DBC.SpellItemEnchantment.add(9999)
+  special.Name.enGB.set('[special]')
+  special.Flags.set(0)
+  special.Effect.set([0, 0, 0, 0, 0])
+  special.ItemVisual.set(0)
+  for (let i = 0; i <= 99999; i++) {
+    const enchant = std.DBC.SpellItemEnchantment.add(9996 + i)
+    const prop = std.DBC.ItemRandomProperties.add(10000 + i)
+    enchant.Name.enGB.set('[placeholder ' + i + ']')
+    enchant.Flags.set(0)
+    enchant.Effect.set([0, 0, 0, 0, 0])
+    enchant.ItemVisual.set(0)
+    prop.Name.set('')
+    prop.Name2.enGB.set('')
+    prop.Enchantment.set([i])
+  }
+}
+
+CreatePlaceholderEnchants()
+
+// ability
+// passive
+// utility
+
+// FIXME: move this
+function StarterSpells ($: Builder) {
+  const ability = $.std.DBC.SkillLine.add(9000)
+  const passive = $.std.DBC.SkillLine.add(9001)
+  const utility = $.std.DBC.SkillLine.add(9002)
+  ability.DisplayName.enGB.set('Ability')
+  passive.DisplayName.enGB.set('Passive')
+  utility.DisplayName.enGB.set('Utility')
+  $.std.Spells.load(5938).SkillLines.clearClass('ROGUE')
+  $.std.Spells.load(5938).SkillLines.enable('ROGUE', 'GNOME')
+  $.std.Spells.load(5938).SkillLines.add(9000)
+  $.std.DBC.SkillLineAbility.add(90000, {
+    Spell: 5938,
+    RaceMask: 1791,
+    ClassMask: 8,
+    AcquireMethod: 2,
+    SkillLine: 9000,
+  })
+  $.std.DBC.SkillLineAbility.queryAll({}).forEach(v => v.CharacterPoints)
+  // console.log('skill line abilities')
+  // $.std.DBC.SkillLineAbility.queryAll({}).forEach(v => {
+  //   const spell = $.std.Spells.load(v.Spell.get())
+  //   if (spell && spell.Name && spell.Name.enGB)
+  //     console.log(spell.Name.enGB.get())
+  // })
+
+  // console.log('skill lines')
+  // $.std.DBC.SkillLine.queryAll({}).forEach(v => console.log(v.DisplayName.enGB.get()))
+}
 
