7bd7b1dd743e8f2afcdf5f7388cbb9b588e34025
nl <nldev00@pm.me>
2022-03-06 15:51:12 -0600

    update

--- livescripts/livescripts.ts
+++ livescripts/livescripts.ts
@@ -19,11 +19,19 @@ export function Main (events: TSEvents) {
     if (effect.GetID() === 8326) {
       const player = application.GetTarget().ToPlayer()
       const corpse = player.GetCorpse()
-      player.Teleport(corpse.GetMapID(), corpse.GetX(), corpse.GetY(), corpse.GetZ(), corpse.GetO())
+      player.AddTimer(20, -1, (o, t) => {
+        const p = o.ToPlayer()
+        if (p.IsNull())
+          t.Stop()
+        if (!corpse.IsNull()) {
+          player.SendBroadcastMessage('hello')
+          // player.Teleport(corpse.GetMapID(), corpse.GetX(), corpse.GetY(), corpse.GetZ(), corpse.GetO())
+          t.Stop()
+        }
+      })
     }
   })
   events.Player.OnChat((player, _, msg) => {
-    console.log(msg.get())
     if (msg.get() === 'dead') {
       // player.AddAura(8326, player)
     }
