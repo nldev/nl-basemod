b65961864b98215f49592b8d0cde186fc9205457
nl <nldev00@pm.me>
2022-03-05 19:10:03 -0600

    update

--- livescripts/livescripts.ts
+++ livescripts/livescripts.ts
@@ -739,7 +739,13 @@ function LevelingSystem (events: TSEvents) {
   })
 
   events.Player.OnLevelChanged((player, oldLevel) => {
-    SetAbilities(player)
+    const power = player.GetPowerType()
+    // rage
+    if (power === 1)
+      player.SetPower(player.GetMaxPower(1), 1)
+    // energy
+    if (power === 3)
+      player.SetPower(player.GetMaxPower(3), 3)
   })
 }
 
