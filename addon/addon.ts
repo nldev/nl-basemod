74d1402f94b42468136f51c3563e04f258c2ce4f
nl <nldev00@pm.me>
2022-03-08 17:44:23 -0600

    update

--- addon/addon.ts
+++ addon/addon.ts
@@ -3,9 +3,7 @@ import { Talents } from './basemod/talents'
 import { EasyLoot } from './basemod/easy-loot'
 import { Chests } from './basemod/chests'
 
-console.log('test')
 export const app = new App(app => {
-  console.log('my eyes')
   Talents()
   EasyLoot()
   Chests()
