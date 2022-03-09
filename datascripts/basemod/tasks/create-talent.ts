b3eca518e48b3e2a91b565fb539df86027e6b453
nl <nldev00@pm.me>
2022-03-09 04:18:06 -0600

    update

--- datascripts/basemod/tasks/create-talent.ts
+++ datascripts/basemod/tasks/create-talent.ts
@@ -19,6 +19,7 @@ export class CreateTalent extends NWTask {
   static readonly id = CREATE_TALENT_TASK
 
   setup () {
+    this.builder.dbc.Talent.queryAll({}).forEach(t => t.delete())
     this.builder.std.DBC.Talent.queryAll({}).forEach(talent => {
       talent.PrereqRank.set([0, 0, 0, 0, 0, 0, 0, 0])
       talent.Flags.set(0)
