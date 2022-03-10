b25aa8715e62d90e6db6cfff02d9089cf89c8981
nl <nldev00@pm.me>
2022-03-10 00:41:51 -0600

    update

--- addon/basemod/systems.ts
+++ addon/basemod/systems.ts
@@ -6,12 +6,6 @@ export const Systems: Component = () => {
   const a = Frame({ name: 'systems' })
   const b = Frame({ name: 'systems-inner', parent: a })
 
-  const dropdown = Frame({ name: 'systems-dropdown', parent: b })
-  dropdown.ref.SetPoint('TOPLEFT')
-  UIDropDownMenu_Initialize(dropdown.ref, (self, level, menuList) => {
-  })
-  UIDropDownMenu_AddButton({ text: 'hello', checked: true })
-
   a.ref.SetSize(300, 400)
   a.ref.SetBackdrop(BASE_BACKDROP)
   a.ref.SetBackdropColor(0, 0, 0, 1)
@@ -19,6 +13,13 @@ export const Systems: Component = () => {
   b.ref.SetPoint('CENTER')
 
   Movable(a)
+
+  const dropdown = Frame({ name: 'systems-dropdown', parent: b })
+  UIDropDownMenu_Initialize(dropdown.ref, (self, level, menuList) => {
+    UIDropDownMenu_AddButton({ text: 'hello', checked: true })
+    UIDropDownMenu_SetWidth(self, 500)
+    self.SetPoint('TOPLEFT')
+  })
 
   return a
 }
