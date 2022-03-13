60a4950329a7d1e7b4694fd80dd4ce387480fa3e
nl <nldev00@pm.me>
2022-03-12 23:21:18 -0600

    update

--- addon/basemod/components/panel.ts
+++ addon/basemod/components/panel.ts
@@ -6,20 +6,30 @@ import { Movable } from '../utils'
 import { Mapping } from '../types'
 
 export interface PanelOptions extends ComponentOptions {
-  nav: DropdownItemOptions[]
-  pages: Mapping<Component>
+  // FIXME
+  nav?: DropdownItemOptions[]
+  pages?: Mapping<Component>
+
   defaultSelectionId?: string
   isHiddenOnEmpty?: boolean
+  title?: string
 }
 
-export const Panel: Component = options => {
+export const Panel: Component<PanelOptions> = options => {
   const $ = Get()
-  const a = Frame(options)
+  const a: Element<PanelOptions> = Frame(options) as any
   const b = Frame({ name: `${a.ref.GetName()}-inner`, parent: a })
   a.inner = b.ref
 
+  a.ref.SetSize(340, 430)
+  a.ref.SetBackdrop(BASE_BACKDROP)
+  a.ref.SetBackdropColor(0, 0, 0, 1)
+  b.ref.SetSize(300 - 30, 400 - 30)
+  b.ref.SetPoint('CENTER')
+
+  //dropdown
   const dropdown = Dropdown({
-    name: 'test-dropdown',
+    name: `${a.ref.GetName()}-dropdown`,
     width: 150,
     defaultSelectionId: $.store.Get('CHARACTER', 'test-dropdown-id', 'foo'),
     isSelectableEmpty: true,
@@ -48,14 +58,30 @@ export const Panel: Component = options => {
   })
 
   dropdown.ref.SetParent(a.ref)
-  dropdown.ref.SetPoint('TOPLEFT', 0, -40)
+  dropdown.ref.SetPoint('TOPLEFT', 0, 34)
 
+  // title
+  // if (options.title) {
+    const title = Frame({
+      name: `${a.ref.GetName()}-title`,
+    })
 
-  a.ref.SetSize(340, 430)
-  a.ref.SetBackdrop(BASE_BACKDROP)
-  a.ref.SetBackdropColor(0, 0, 0, 1)
-  b.ref.SetSize(300 - 30, 400 - 30)
-  b.ref.SetPoint('CENTER')
+    title.ref.SetParent(a.ref)
+    title.ref.SetSize(140, 30)
+    title.ref.SetBackdrop(BASE_BACKDROP)
+    title.ref.SetBackdropColor(0, 0, 0, 1)
+    title.ref.SetPoint('TOPRIGHT', 0, 35)
+    const titleText = title.ref.CreateFontString(
+      'talent-countertext',
+      'OVERLAY',
+      'GameTooltipText',
+    )
+    titleText.SetParent(title.ref)
+    titleText.SetPoint('CENTER')
+    // FIXME
+    titleText.SetText('basemod v0.1.0')
+    titleText.SetFont('Fonts/FRIZQT__.TTF', 10)
+  // }
 
   Movable(a)
 
