1ebe8fb3aaa7ae5916dac7970206ff0d097bec75
nl <nldev00@pm.me>
2022-03-13 05:41:43 -0500

    update

--- addon/basemod/components/panel.ts
+++ addon/basemod/components/panel.ts
@@ -144,6 +144,8 @@ export const Panel: Component<PanelOptions> = options => {
       dropdown.ref.Show()
       a.ref.Show()
     }
+    console.log($.store.Get('CHARACTER', 'test-panel-visibility'))
+    console.log(typeof $.store.Get('CHARACTER', 'test-panel-visibility'))
   }
 
   title.ref.HookScript('OnEnter', () => title.ref.SetBackdropColor(0.21, 0.49, 1, 1))
@@ -154,7 +156,7 @@ export const Panel: Component<PanelOptions> = options => {
       TogglePanel()
   })
 
-  if (!$.store.Get('CHARACTER', 'test-panel-visibility')) {
+  if ($.store.Get('CHARACTER', 'test-panel-visibility')) {
     dropdown.ref.Hide()
     a.ref.Hide()
   }
