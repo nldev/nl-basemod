47e16aff7106169ddd24115789f15830dad59c6b
nl <nldev00@pm.me>
2022-03-12 22:45:15 -0600

    update

--- addon/addon.ts
+++ addon/addon.ts
@@ -12,36 +12,6 @@ export const app = new App(app => {
   Chests()
 
   const panel = Panel({ name: 'panel', parent: app.root })
-  const dropdown = Dropdown({
-    name: 'test-dropdown',
-    parent: panel,
-    defaultSelectionId: app.store.Get('CHARACTER', 'test-dropdown-id', 'foo'),
-    isSelectableEmpty: true,
-    // isTriggerOnInit: true,
-    // isTriggerOnReselect: false,
-    emptyText: 'select a thing',
-    items: [
-      {
-        id: 'foo',
-        text: 'Foo',
-        value: 1,
-      },
-      {
-        id: 'bar',
-        text: 'Bar',
-        value: 2,
-      },
-      {
-        id: 'baz',
-        text: 'Baz',
-        value: 3,
-        tooltip: 'this has a tooltip',
-      },
-    ],
-    onSelect: ({ id }) => app.store.Set('CHARACTER', 'test-dropdown-id', id),
-  })
-
-  dropdown.ref.SetPoint('TOPLEFT')
 
   let txt = ''
   app.root.ref.SetScript('OnUpdate', () => {
--- addon/basemod/components/panel.ts
+++ addon/basemod/components/panel.ts
@@ -10,14 +10,45 @@ export const Panel: Component = options => {
   const $ = Get()
   const a = Frame(options)
   const b = Frame({ name: `${a.ref.GetName()}-inner`, parent: a })
+  a.inner = b.ref
+
+  const dropdown = Dropdown({
+    name: 'test-dropdown',
+    parent: a,
+    defaultSelectionId: $.store.Get('CHARACTER', 'test-dropdown-id', 'foo'),
+    isSelectableEmpty: true,
+    // isTriggerOnInit: true,
+    // isTriggerOnReselect: false,
+    emptyText: 'select a thing',
+    items: [
+      {
+        id: 'foo',
+        text: 'Foo',
+        value: 1,
+      },
+      {
+        id: 'bar',
+        text: 'Bar',
+        value: 2,
+      },
+      {
+        id: 'baz',
+        text: 'Baz',
+        value: 3,
+        tooltip: 'this has a tooltip',
+      },
+    ],
+    onSelect: ({ id }) => $.store.Set('CHARACTER', 'test-dropdown-id', id),
+  })
+
+  dropdown.ref.SetPoint('TOPLEFT')
+
 
   a.ref.SetSize(340, 430)
   a.ref.SetBackdrop(BASE_BACKDROP)
   a.ref.SetBackdropColor(0, 0, 0, 1)
   b.ref.SetSize(300 - 30, 400 - 30)
   b.ref.SetPoint('CENTER')
-
-  a.inner = b.ref
 
   Movable(a)
 
