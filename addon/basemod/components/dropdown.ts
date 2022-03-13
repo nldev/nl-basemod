a4d78baf5eb0b3ecb7882f773e7a5ba4f3ff2373
nl <nldev00@pm.me>
2022-03-13 01:20:15 -0600

    update

--- addon/basemod/components/dropdown.ts
+++ addon/basemod/components/dropdown.ts
@@ -4,7 +4,7 @@ import { BASE_BACKDROP } from '../constants'
 import { Mapping } from '../types'
 import { Movable } from '../utils'
 
-const AUTOHIDE_TIMER = 1
+const AUTOHIDE_TIMER = 1.2
 
 const DEFAULT_SELECTION = {
   id: '',
