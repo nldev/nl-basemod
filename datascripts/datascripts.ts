fbaa131d6a019dc3ec0a1024496b1129d95a0b10
nl <nldev00@pm.me>
2022-03-14 01:36:57 -0500

    update

--- datascripts/datascripts.ts
+++ datascripts/datascripts.ts
@@ -4,4 +4,7 @@ import { Builder } from './basemod'
 const $ = new Builder()
 
 // TODO:
-// - check 'needs' in templates (use data)
+// - add 'needs' props in templates (use data)
+// - add 'transforms' props in templates
+// - attempt to rerun needs after every Process call
+
