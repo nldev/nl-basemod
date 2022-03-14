2736f8df996850cb63c02da32d3041280781a45b
nl <nldev00@pm.me>
2022-03-14 00:22:12 -0500

    update

--- datascripts/datascripts.ts
+++ datascripts/datascripts.ts
@@ -2,19 +2,3 @@ import { Builder } from './basemod'
 
 const $ = new Builder()
 
-$.ProcessMany<string>({
-  id: 'hello',
-  list: [
-    'hello',
-    'world',
-    'what',
-  ],
-})
-
-const thing = $.Get<TSSpell>('spells', 'fireball')
-
-$.Process<{ thing: string }>({
-  id: 'hello',
-  data: { thing: 'hello' },
-})
-
