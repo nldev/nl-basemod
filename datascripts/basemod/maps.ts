9dba0e4a1af8ec08647bcf6527e11f2da8d42015
nl <nldev00@pm.me>
2022-03-18 17:32:29 -0500

    update

---
+++ datascripts/basemod/maps.ts
@@ -0,0 +1,46 @@
+import { std } from 'wow/wotlk'
+import { Map as TSMap } from 'wow/wotlk/std/Map/Map'
+import { Task } from '.'
+import { AssetOptions, Asset } from './types'
+import { TitleCaseToDashCase } from './utils'
+
+const DEFAULT_MAP = 0
+
+export interface Map extends Asset {
+  asset: TSMap
+}
+
+export interface MapOptions extends AssetOptions {
+  asset?: TSMap
+}
+
+export interface CreateMapConfig {
+  // isPrefix?: boolean
+}
+
+export const CreateSpell: Task<MapOptions, CreateMapConfig> = {
+  id: 'create-spell',
+  identify: ($, config, options) => {
+    if (!config.data.baseId)
+      throw new Error('create-map templates require a baseId to automatically assign ID')
+
+    return TitleCaseToDashCase(std.Maps.load(config.data.baseId).Name.enGB.get())
+  },
+  setup: ($, config) => {},
+  process: ($, template, config) => {
+    const baseId = template.data.baseId || DEFAULT_MAP
+    const item: Map = {
+      baseId,
+      id: template.id,
+      isModify: (typeof template.data.isModify === 'boolean')
+        ? template.data.isModify
+        : false,
+      asset: template.data.isModify
+        ? std.Maps.load(baseId)
+        : std.Maps.create($.Mod, template.id),
+    }
+
+    $.Set('spells', template.id, item)
+  },
+}
+
