e8d4c67abf446938eb26f1592ce72212419eb94f
nl <nldev00@pm.me>
2022-03-18 20:00:01 -0500

    update

--- datascripts/datascripts.ts
+++ datascripts/datascripts.ts
@@ -1,13 +1,16 @@
 import { Builder } from './basemod'
+import { SQLTable } from './basemod/types'
 import { TalentOptions } from './basemod/talents'
 import { Autolearn } from './basemod/autolearn'
 import { Map, MapOptions } from './basemod/maps'
+import { TABLES } from './config/tables'
 import { TALENTS } from './config/talents'
 import { AUTOLEARN } from './config/autolearn'
 import { MAPS } from './config/maps'
 import { std } from 'wow/wotlk'
 
 new Builder($ => {
+  $.ProcessMany<SQLTable>(TABLES)
   $.ProcessMany<TalentOptions>(TALENTS)
   $.ProcessMany<Autolearn>(AUTOLEARN)
   $.ProcessMany<MapOptions>(MAPS)
