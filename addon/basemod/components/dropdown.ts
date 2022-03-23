3a8fb836f5a411d41f6abdd2c715c7cc74e1da63
nl <nldev00@pm.me>
2022-03-23 00:32:19 -0500

    update

--- addon/basemod/components/dropdown.ts
+++ addon/basemod/components/dropdown.ts
@@ -272,8 +272,8 @@ export const Dropdown: Component<DropdownOptions, DropdownState> = options => {
 
     a.state.length = list.state.items.length
 
-    menu.ref.SetHeight((a.state.length * 30) + 20)
-    list.ref.SetHeight((a.state.length * 30) + 20)
+    menu.ref.SetHeight((a.state.length * 30) + 80)
+    list.ref.SetHeight((a.state.length * 30) + 80)
 
     autohide[`item-${options.id}`] = false
     items[options.id] = {
