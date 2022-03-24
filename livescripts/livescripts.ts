e28f120e87aa752917439d14c5de09cb824f75cd
nl <nldev00@pm.me>
2022-03-24 01:16:22 -0500

    update

--- livescripts/livescripts.ts
+++ livescripts/livescripts.ts
@@ -24,7 +24,7 @@ export function Main (events: TSEvents) {
     for (let i = 0; i <= 15; i++) {
       const item = sender.GetItemByPos(255, 23 + i)
       if (!item.IsNull())
-        sender.RemoveItem(item)
+        sender.RemoveItem(item, item.GetCount())
     }
   })
 
