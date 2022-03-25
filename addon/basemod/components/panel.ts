458314faef303bb65089218492e6789d0e4c7365
nl <nldev00@pm.me>
2022-03-24 19:14:28 -0500

    update

--- addon/basemod/components/panel.ts
+++ addon/basemod/components/panel.ts
@@ -30,7 +30,7 @@ export const Section: Component<SectionOptions> = options => {
 
   // position based on previous
   if (options.previous)
-    p.ref.SetPoint('TOPLEFT', options.previous.ref, 'BOTTOMLEFT', 0, -16)
+    p.ref.SetPoint('TOPLEFT', options.previous.ref, 'BOTTOMLEFT', 0, -18)
 
   // inner
   const f = Frame({ name: `${options.name}-inner`, parent: p })
