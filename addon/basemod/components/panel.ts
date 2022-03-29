963d7ebf54668389e740afc3958033819b25666f
nl <nldev00@pm.me>
2022-03-28 19:11:50 -0500

    update

--- addon/basemod/components/panel.ts
+++ addon/basemod/components/panel.ts
@@ -2,81 +2,11 @@ import { Get } from '../app'
 import { Component, ComponentOptions, Frame, Element } from '../app'
 import { Dropdown, DropdownItemOptions } from './dropdown'
 import { BASE_BACKDROP } from '../constants'
-import { Movable, rgb } from '../utils'
-import { Mapping, Rgb } from '../types'
+import { Movable } from '../utils'
+import { Mapping } from '../types'
 import { Talents } from '../talents'
 import { Scroll } from './scroll'
-
-export interface SectionOptions extends ComponentOptions {
-  name: string
-  parent: Element<any, any>
-  height: number
-  previous?: Element<any, any>
-  x?: number
-  y?: number
-  title?: string
-  bg?: boolean
-  color?: Rgb
-}
-
-export const Section: Component<SectionOptions> = options => {
-  const p = Frame(options)
-
-  // padding
-  p.ref.SetHeight(options.bg ? options.height : (options.height - 20))
-  p.ref.SetWidth(options.width || (options.parent.inner.GetWidth() - 6))
-
-  // x, y
-  const x = options.x || 0
-  const y = options.y || -12
-
-  // position based on y
-  p.ref.SetPoint('TOPLEFT', x, y)
-
-  // position based on previous
-  if (options.previous)
-    p.ref.SetPoint('TOPLEFT', options.previous.ref, 'BOTTOMLEFT', x, -26)
-
-  // inner
-  const f = Frame({ name: `${options.name}-inner`, parent: p })
-  if (options.bg) {
-    f.ref.SetHeight(p.ref.GetHeight() - 20)
-    f.ref.SetWidth(p.ref.GetWidth() - 20)
-  } else {
-    f.ref.SetWidth(p.ref.GetWidth() + 4)
-    f.ref.SetHeight(p.ref.GetHeight() - 4)
-  }
-  f.ref.SetPoint('CENTER')
-  p.inner = f.ref
-
-  // title
-  if (options.title) {
-    const text = p.ref.CreateFontString(
-      `${p.ref.GetName()}-title`,
-      'OVERLAY',
-      'GameTooltipText',
-    )
-    text.SetFont('Fonts/FRIZQT__.TTF', 12)
-    text.SetText(options.title)
-    text.SetPoint('TOPLEFT', 0, 15)
-  }
-
-  // color
-  if (options.bg) {
-    p.ref.SetBackdrop({
-      ...BASE_BACKDROP,
-      bgFile: options.color ? 'Interface/Tooltips/UI-Tooltip-Background' : '',
-      edgeFile: options.bg ? BASE_BACKDROP.edgeFile : '',
-    })
-
-    p.ref.SetBackdropColor(0, 0, 0, 0)
-
-    if (options.color)
-      p.ref.SetBackdropColor(...rgb(...options.color), 1)
-  }
-
-  return p
-}
+import { Section } from './section'
 
 export const DevTools: Component = options => {
   const f = Frame({ name: 'devtools', ...options })
