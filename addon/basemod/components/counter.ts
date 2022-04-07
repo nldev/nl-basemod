fd8600d3f0787feeef9696880512a526bab5aaac
nl <nldev00@pm.me>
2022-04-07 02:44:51 -0500

    update

--- addon/basemod/components/counter.ts
+++ addon/basemod/components/counter.ts
@@ -1,15 +1,6 @@
-import { Get } from '../app'
 import { Component, ComponentOptions, Frame, Element } from '../app'
-import { Dropdown, DropdownItemOptions } from './dropdown'
 import { BASE_BACKDROP } from '../constants'
-import { Movable } from '../utils'
-import { Mapping } from '../types'
-import { Talents } from '../talents'
-import { Scroll } from './scroll'
-import { Section } from './section'
 import { Button } from './button'
-import { Rgb } from '../types'
-import { rgb } from '../utils'
 
 export interface CounterOptions extends ComponentOptions {
   width?: number
