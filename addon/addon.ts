e4f2b68d64a9f0c9c6a5c5c19dee7c56c0963e36
nl <nldev00@pm.me>
2022-04-07 02:38:14 -0500

    update

--- addon/addon.ts
+++ addon/addon.ts
@@ -1,5 +1,6 @@
-import { App } from './basemod/app'
-// import { Talents } from './basemod/talents'
+import { App, Frame } from './basemod/app'
+import { Talents } from './basemod/talents'
+import { DevTools } from './basemod/panels/dev-tools'
 import { EasyLoot } from './basemod/easy-loot'
 import { Chests } from './basemod/chests'
 import { AllChildren } from './basemod/utils'
@@ -9,9 +10,104 @@ import { Dropdown } from './basemod/components/dropdown'
 export const app = new App(app => {
   EasyLoot()
   Chests()
+})
 
-  const panel = Panel({ name: 'panel', parent: app.root })
+const panel = Panel({
+  title: 'basemod v0.1.0',
+  components: {
+    'dev-tools': DevTools,
+    'dev-equip': Frame,
+    'dev-consume': Frame,
+    'profile': Frame,
+    'abilities': Talents,
+    'jobs': Frame,
+    'lfg': Frame,
+    'stash': Frame,
+    'market': Frame,
+    'clan': Frame,
+    'mail': Frame,
+    'notes': Frame,
+    'settings': Frame,
+    'feedback': Frame,
+    'support': Frame,
+  },
+  nav: [
+   {
+     id: 'dev-tools',
+     text: '[dev] General',
+   },
+   {
+     id: 'dev-equip',
+     text: '[dev] Equipment',
+   },
+   {
+     id: 'dev-consume',
+     text: '[dev] Consumables',
+   },
+   {
+     id: 'profile',
+     text: 'Profile',
+     disabled: true,
+   },
+   {
+     id: 'abilities',
+     text: 'Abilities',
+   },
+   {
+     id: 'jobs',
+     text: 'Jobs',
+     disabled: true,
+   },
+   {
+     id: 'lfg',
+     text: 'Group Finder',
+     disabled: true,
+   },
+   {
+     id: 'stash',
+     text: 'Stash',
+     disabled: true,
+   },
+   {
+     id: 'market',
+     text: 'Market',
+     disabled: true,
+   },
+   {
+     id: 'clan',
+     text: 'Clan',
+     disabled: true,
+   },
+   {
+     id: 'mail',
+     text: 'Mail',
+     disabled: true,
+   },
+   {
+     id: 'journal',
+     text: 'Journal',
+     disabled: true,
+   },
+   {
+     id: 'settings',
+     text: 'Settings',
+     disabled: true,
+   },
+   {
+     id: 'feedback',
+     text: 'Feedback',
+     disabled: true,
+   },
+   {
+     id: 'support',
+     text: 'Support',
+     disabled: true,
+   },
+  ],
+  pages: {},
+})
 
+const test = () => {
   let txt = ''
   app.root.ref.SetScript('OnUpdate', () => {
     const f = GetMouseFocus()
@@ -26,5 +122,6 @@ export const app = new App(app => {
       }
     }
   })
-})
+}
+test()
 
