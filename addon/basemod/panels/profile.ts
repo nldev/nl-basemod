9edcc474526521e23105d0f934606671c1c49988
nl <nldev00@pm.me>
2022-04-09 16:21:28 -0500

    update

--- addon/basemod/panels/profile.ts
+++ addon/basemod/panels/profile.ts
@@ -11,58 +11,64 @@ import { Button } from '../components/button'
 import { Counter } from '../components/counter'
 import { Grid } from '../components/grid'
 import { Checkbox } from '../components/checkbox'
+import { Input } from '../components/input'
 
 export const Profile: Component = options => {
-  const f = Frame({ name: 'profile', ...options })
-  f.ref.SetSize(290, 360)
-  f.ref.SetPoint('CENTER')
+  const frame = Frame({ name: 'profile', ...options })
+  frame.ref.SetSize(290, 360)
+  frame.ref.SetPoint('CENTER')
 
-  const scroll = Scroll({ name: 'profile-scroll', parent: f, height: 300 })
+  const scroll = Scroll({ name: 'profile-scroll', parent: frame, height: 300 })
 
   // name
-  const name = Section({
+  const a = Section({
     name: 'name',
     title: 'Name',
     parent: scroll,
     height: 50,
   })
+  const name = Input({
+    name: 'name-input',
+    parent: a,
+  })
+  name.ref.SetPoint('TOPLEFT')
 
   // class
-  const cls = Section({
+  const b = Section({
     name: 'class',
     title: 'Class',
     parent: scroll,
-    previous: name,
+    previous: a,
     height: 50,
   })
 
   // player-status
-  const playerStatus = Section({
+  const c = Section({
     name: 'player-status',
     title: 'Player Status',
     parent: scroll,
-    previous: cls,
+    previous: b,
     height: 50,
   })
 
   // character-status
-  const characterStatus = Section({
+  const d = Section({
     name: 'character-status',
     title: 'Character Status',
     parent: scroll,
-    previous: playerStatus,
+    previous: c,
     height: 50,
   })
 
   // about
-  const about = Section({
+  const e = Section({
     name: 'about',
     title: 'About',
     parent: scroll,
-    previous: characterStatus,
+    previous: d,
     height: 50,
   })
 
-  return f
+  return frame
 }
 
