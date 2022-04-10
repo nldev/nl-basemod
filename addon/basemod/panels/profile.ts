bd9f91d5e8c47df135da7c7aa559a5f60b25c009
nl <nldev00@pm.me>
2022-04-10 05:17:53 -0500

    update

--- addon/basemod/panels/profile.ts
+++ addon/basemod/panels/profile.ts
@@ -140,10 +140,8 @@ export const Profile: Component = options => {
     height: 250,
     parent: d,
     initial: $.store.Get('CHARACTER', 'rp-about', ''),
-    onAccept: text => {
-      console.log(text)
-      $.store.Set('CHARACTER', 'rp-about', text)
-    },
+    onAccept: text =>
+      $.store.Set('CHARACTER', 'rp-about', text),
     onChange: text => md(text),
   })
   editor.ref.SetPoint('TOPLEFT')
