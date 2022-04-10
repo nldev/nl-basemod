7aa2c0c16224e23736ac837505491fa54bed34ba
nl <nldev00@pm.me>
2022-04-10 01:50:32 -0500

    update

--- addon/basemod/panels/profile.ts
+++ addon/basemod/panels/profile.ts
@@ -161,7 +161,7 @@ export const Profile: Component = options => {
     width: e.inner.GetWidth(),
     height: e.inner.GetHeight(),
   })
-  view.ref.SetPoint('BOTTOMLEFT', 0, -20)
+  view.ref.SetPoint('TOPLEFT', 0, -20)
   const md = Markdown(view.ref)
 
   return frame
